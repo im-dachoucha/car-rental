3bf2a669cc57519d56d98f49621503b0b0f8529f
abderrahmane <abderrahamane7@gmail.com>
2021-12-23 16:33:29 +0100

    contact page completed

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("form.contact-form");
+form.addEventListener("submit", (e) => {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const subject = document.getElementById("subject");
+  const msg = document.getElementById("msg");
+  e.preventDefault();
+  alert(
+    `name : ${name.value}\nemail : ${email.value}\nsubject : ${subject.value}\mmessage : ${msg.value}`
+  );
+  console.log(
+    `name : ${name.value}\nemail : ${email.value}\nsubject : ${subject.value}\mmessage : ${msg.value}`
+  );
+});
